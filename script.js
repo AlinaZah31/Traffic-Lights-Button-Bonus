4bc5ed8c37e025e83140327627bad24942c1d556
AlinaZah31 <alina_shuvi@yahoo.com>
2024-05-26 23:04:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let colors = ["orange", "green", "red"]; 
 let currentIndex = 0;
+
 function setColor() {
     const button = document.getElementById("btn");
     button.style.backgroundColor = colors[currentIndex];
     currentIndex = (currentIndex + 1) % colors.length;
 }
+
 setInterval(setColor, 10000);
 
